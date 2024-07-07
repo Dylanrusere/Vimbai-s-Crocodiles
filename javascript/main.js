046e5b30d43bb37603f1287a577de8c3222b957e
Dylanrusere <dylaneight01@gmail.com>
2024-07-07 13:15:20 +0200

    on counter

--- javascript/main.js
+++ javascript/main.js
@@ -100,3 +100,45 @@ carouselContainer.addEventListener("mouseenter", pauseCarousel);
 carouselContainer.addEventListener("mouseleave", startCarousel);
 
 startCarousel();
+
+// Counter Section
+let targetNumber1 = 29;
+let targetNumber2 = 19;
+let targetNumber3 = 49;
+let counter1 = 0;
+let counter2 = 0;
+let counter3 = 0;
+let counterElement1 = document.getElementById("counter_clients");
+let counterElement2 = document.getElementById("counter_properties");
+let counterElement3 = document.getElementById("counter_available");
+
+function incrementCounter1() {
+    counter1++;
+    counterElement1.textContent = counter1;
+
+    if (counter1 <= targetNumber1) {
+      setTimeout(incrementCounter1, 150);
+    }
+  }
+
+  function incrementCounter2() {
+    counter2++;
+    counterElement2.textContent = counter2;
+
+    if (counter2 <= targetNumber2) {
+      setTimeout(incrementCounter2, 200);
+    }
+  }
+
+  function incrementCounter3() {
+    counter3++;
+    counterElement3.textContent = counter3;
+
+    if (counter3 <= targetNumber3) {
+      setTimeout(incrementCounter3, 80);
+    }
+  }
+
+incrementCounter1();
+incrementCounter2();
+incrementCounter3();
